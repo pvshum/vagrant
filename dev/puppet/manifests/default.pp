251d60e87006435b527514afa99053c639cfda7b
Wee Keat Chin <weekeat@visualdensity.com>
2013-04-12 09:45:59 +1000

    update

--- dev/puppet/manifests/default.pp
+++ dev/puppet/manifests/default.pp
@@ -1,12 +1,14 @@
 class system-setup {
+    exec { "import-gpg":
+        command => "/usr/bin/wget -q http://www.dotdeb.org/dotdeb.gpg -O -| /usr/bin/apt-key add -"
+    }
+
     file { "/etc/apt/sources.list":
         ensure => file,
         owner => root,
         group => root,
         source => "puppet:///modules/system/sources.list",
-    }
-    exec { "import-gpg":
-        command => "/usr/bin/wget -q http://www.dotdeb.org/dotdeb.gpg -O -| /usr/bin/apt-key add -"
+        require => Exec["import-gpg"],
     }
 
     exec { "/usr/bin/apt-get update":
@@ -15,6 +17,11 @@ class system-setup {
 }
 
 class essentials {
+    package { "vim-nox":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
     package { "git":
         ensure => present,
         require => [Exec["/usr/bin/apt-get update"]],
@@ -23,6 +30,56 @@ class essentials {
     package { "htop":
         ensure => present,
         require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "curl":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "ntp":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "ntpdate":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "iotop":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "iftop":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "tcpdump":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "zsh":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "ruby":
+        ensure => present,
+        require => [Exec["/usr/bin/apt-get update"]],
+    }
+
+    package { "rubygems":
+        ensure => present,
+        require => [Package["ruby"]],
+    }
+
+    package { "rake":
+        ensure => present,
+        require => [Package["ruby"]],
     }
 }
 
@@ -33,7 +90,19 @@ class apache {
     }
     package { "libapache2-mod-php5":
         ensure => present,
-        require => [Exec["/usr/bin/apt-get update"]],
+        require => [Package["apache2"]],
+    }
+
+    service { "apache2":
+        ensure => running,
+        hasstatus => true,
+        hasrestart => true,
+        require => Package["apache2"],
+    }
+
+    exec { "restart-apache2":
+        command => "/etc/init.d/apache2 restart",
+        refreshonly => true,
     }
 }
 
@@ -41,26 +110,42 @@ class php {
     package { "php5-common":
         ensure => present,
         require => [Exec["/usr/bin/apt-get update"]],
+        notify => Exec["restart-apache2"],
     }
     package { "php5-suhosin":
         ensure => present,
-        require => [Exec["/usr/bin/apt-get update"]],
+        require => [Package["php5-common"]],
+        notify => Exec["restart-apache2"],
     }
     package { "php5-mysql":
         ensure => present,
-        require => [Exec["/usr/bin/apt-get update"]],
+        require => [Package["php5-common"]],
+        notify => Exec["restart-apache2"],
     }
     package { "php5-memcache":
         ensure => present,
-        require => [Exec["/usr/bin/apt-get update"]],
+        require => [Package["php5-common"]],
+        notify => Exec["restart-apache2"],
     }
     package { "php5-mcrypt":
         ensure => present,
-        require => [Exec["/usr/bin/apt-get update"]],
+        require => [Package["php5-common"]],
+        notify => Exec["restart-apache2"],
     }
     package { "php5-xdebug":
         ensure => present,
-        require => [Exec["/usr/bin/apt-get update"]],
+        require => [Package["php5-common"]],
+        notify => Exec["restart-apache2"],
+    }
+    package { "php5-intl":
+        ensure => present,
+        require => [Package["php5-common"]],
+        notify => Exec["restart-apache2"],
+    }
+    package { "php5-sqlite":
+        ensure => present,
+        require => [Package["php5-common"]],
+        notify => Exec["restart-apache2"],
     }
 }
 
@@ -80,5 +165,5 @@ node default {
     include essentials
     include apache
     include php
-    include mysql
+#    include mysql
 }
